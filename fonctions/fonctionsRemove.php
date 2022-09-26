4c5ceaab8bb48c8b656d77b3abeb2c345917888e
Chaamoxs <chamoxs57@gmail.com>
2022-09-26 12:05:16 -0400

    correction de la fonction pour retirer du panier

--- fonctions/fonctionsRemove.php
+++ fonctions/fonctionsRemove.php
@@ -1,26 +1,18 @@
 <?php
-	if(isset($_POST["item"]) && isset($_POST["pos"])){
+	if(isset($_POST["item"]) && isset($_POST["pos"])){ 
 		$arr1 = array();
 		$trouve = false;
-		if(isset($_COOKIE["panier"])){
-		$arr = json_decode($_COOKIE["panier"],true);
-		$x = 0;	
-			foreach($arr as $item){
-				
-				if(($_POST["item"] == $item) && ($x == $_POST["pos"]) && $trouve == false){
+		if(isset($_COOKIE["panier"])){ 
+			$arr = json_decode($_COOKIE["panier"],true);
+			foreach($arr as $item){			
+				if(($_POST["item"] == $item) && $trouve == false){ 
 					$trouve = true;
 				}else{
-					$arr1[] = $item;
-					$x++;
+					//$arr1[] = $item;
+					array_push($arr1,$item);
 				}				
-				
 			}
 		}
-		
-		if($trouve){
-			setcookie('panier',json_encode($arr1),time() + (60*30),"/");
-		}else{
-			setcookie("panier", "", time()-3600,"/");
-		}			
+		setcookie('panier',json_encode($arr1),time() + (60*30),"/");
 	}
 ?>
